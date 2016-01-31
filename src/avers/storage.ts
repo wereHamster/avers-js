1f62188edd91276c5e60a5fae83934b7014e1e48
Tomas Carnecky <tomas.carnecky@gmail.com>
2016-01-31 16:37:46 +0100

    Use the feed endpoint to stream changes to the client

--- src/avers/storage.ts
+++ src/avers/storage.ts
@@ -51,6 +51,11 @@ export class Handle {
     staticCache    = new Map<Symbol, Map<string, StaticE<any>>>();
     ephemeralCache = new Map<Symbol, Map<string, EphemeralE<any>>>();
 
+    feedSocket : WebSocket = undefined;
+    // ^ A WebSocket connected to the feed through which the client receives
+    // change notifications (eg. new patches).
+
+
     constructor
       ( public apiHost : string
         // ^ The hostname where we can reach the Avers API server. Leave
@@ -182,6 +187,95 @@ localChanges(h: Handle): { obj: Editable<any>; changes: Operation[]; }[] {
 }
 
 
+// changeFeedSubscription
+// -----------------------------------------------------------------------------
+//
+// Change the feed subscription. Opens the websocket if not already open.
+
+function
+changeFeedSubscription(h: Handle, json) {
+    if (h.feedSocket === undefined) {
+        h.feedSocket = h.createWebSocket('/feed');
+
+        h.feedSocket.addEventListener('message', msg => {
+            try {
+                applyChange(h, JSON.parse(msg.data));
+            } catch (e) {
+                console.error('changeFeedSubscription: error when parsing message', e);
+            }
+        });
+    }
+
+    if (h.feedSocket.readyState === WebSocket.OPEN) {
+        h.feedSocket.send(JSON.stringify(json));
+    } else {
+        h.feedSocket.addEventListener('open', function onOpen() {
+            h.feedSocket.send(JSON.stringify(json));
+            h.feedSocket.removeEventListener('open', onOpen);
+        });
+    }
+}
+
+
+function
+applyChange(h: Handle, change) {
+    modifyHandle(h, mkAction(`applyFeedChange(${change.type})`, h => {
+        let {type,content} = change;
+
+        if (type === 'patch') {
+            let patch = parsePatch(content);
+            updateEditable(h, patch.objectId, obj => {
+                applyPatches(obj, [patch]);
+                initContent(obj);
+            });
+        } else {
+            console.info('applyChange: Unhandled type: ' + type);
+        }
+    }));
+}
+
+
+
+// applyPatches
+// -----------------------------------------------------------------------------
+//
+// The given patches MUST have consecutive revisionIds!
+
+function
+applyPatches(obj: Editable<any>, patches: Patch[]): void {
+    if (obj.shadowContent === undefined) {
+        // We simply ignore any attempt to apply patches to an Editable which
+        // is not resolved.
+        //
+        // We could store the patches somewhere and attempt to apply them once
+        // the Editable is resolved. But that would require careful changes to
+        // multiple parts of the code. Given how low the chance is for that to
+        // happen and cause problems, we decided to not handle that situation.
+
+        return;
+    }
+
+    let applicablePatches = patches.filter(p => p.revisionId > obj.revisionId);
+    if (applicablePatches.length === 0 || applicablePatches[0].revisionId !== obj.revisionId + 1) {
+        // The first patch is not one that can be applied directly on the
+        // Editable. This means there is a gap between the first patche we have
+        // and the state of the Editable.
+        //
+        // We could consider storing the patches somewhere, reload the ones
+        // which are missing and then apply them all. But, again, the chance
+        // for that to happen is so low that we don't bother.
+
+        return;
+    }
+
+    obj.revisionId += applicablePatches.length;
+    obj.shadowContent = applicablePatches.reduce((c, patch) => {
+        let op = patch.operation;
+        return applyOperation(c, op.path, op);
+    }, obj.shadowContent);
+}
+
+
 
 // mkEditable
 // -----------------------------------------------------------------------
@@ -198,6 +292,8 @@ mkEditable<T>(h: Handle, id: string): Editable<T> {
         obj.changeListener = mkChangeListener(h, id);
 
         h.objectCache.set(id, Object.freeze(obj));
+
+        changeFeedSubscription(h, ['+', id]);
     }
 
     return obj;
@@ -539,6 +635,10 @@ deleteObject(h: Handle, id: string): Promise<void> {
 }
 
 function initContent(obj: Editable<any>): void {
+    if (obj.shadowContent === undefined) {
+        return;
+    }
+
     if (obj.content) {
         detachChangeListener(obj.content, obj.changeListener);
     }
@@ -704,29 +804,7 @@ saveEditable(h: Handle, objId: string): void {
                 // server.
                 obj.submittedChanges = [];
 
-
-                // Apply patches which the server sent us to the shadow content.
-                // Here we have to be careful which patches to apply. The server
-                // patches may have been delivered to us via another channel,
-                // such as a WebSocket connection.
-                //
-                // The revIds in previousPatches and resultingPatches are
-                // guaranteed to be continuous, but we may have to drop some
-                // patches from the head of the list if those revision were
-                // already applied.
-
-                let serverPatches = [].concat(body.previousPatches, body.resultingPatches).filter(patch => {
-                    return patch.revisionId > obj.revisionId;
-                });
-
-                obj.revisionId += serverPatches.length;
-                obj.shadowContent = serverPatches.reduce((c, patch) => {
-                    let op = patch.operation;
-                    return applyOperation(c, op.path, op);
-                }, obj.shadowContent);
-
-
-                // Re-initialize the local content.
+                applyPatches(obj, [].concat(body.previousPatches, body.resultingPatches));
                 initContent(obj);
             });
         }));
